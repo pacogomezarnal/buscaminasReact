24499725b49b08e4ee00867cb24a6124ab22c829
Paco Mastermind <paco@mastermind.ac>
2023-09-07 13:28:52 +0200

    Conditional rendering

--- src/Celda.js
+++ src/Celda.js
@@ -1,9 +1,31 @@
 import './estilos.css'
 
 export default function Celda({valor}){
-    return(
-        <button className="border border-2 border-dark-subtle fs-2 fw-bold text-primary" style={{width:50}}>
-            {valor}
-        </button>
-    );
+    
+        if (valor==2) {
+            return(
+                <button className="border border-2 border-dark-subtle fs-2 fw-bold text-success" style={{width:50}}>
+                    {valor}
+                </button>
+            )
+        }else if(valor==1){
+            return(
+                <button className="border border-2 border-dark-subtle fs-2 fw-bold text-primary" style={{width:50}}>
+                    {valor}
+                </button>
+            )
+        }else if(valor==0){
+            return(
+                <button className="border border-2 border-dark-subtle fs-2 fw-bold text-primary" style={{width:50}}>
+                    &nbsp;
+                </button>
+            )            
+        }else{
+            return(
+                <button className="border border-2 border-dark-subtle fs-2 fw-bold text-dark" style={{width:50}}>
+                    {valor}
+                </button>
+            )            
+        }
+
 }

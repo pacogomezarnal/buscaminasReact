e53c35ebdf6c58335c7b11c0fb979d97601f234d
Paco Mastermind <paco@mastermind.ac>
2023-09-07 13:48:23 +0200

    Añadiendo estado

--- src/Celda.js
+++ src/Celda.js
@@ -1,31 +1,42 @@
+import { useState } from 'react';
 import './estilos.css'
 
 export default function Celda({valor}){
-    
-        if (valor==2) {
-            return(
-                <button className="border border-2 border-dark-subtle fs-2 fw-bold text-success" style={{width:50}}>
-                    {valor}
-                </button>
-            )
-        }else if(valor==1){
-            return(
-                <button className="border border-2 border-dark-subtle fs-2 fw-bold text-primary" style={{width:50}}>
-                    {valor}
-                </button>
-            )
-        }else if(valor==0){
+        const [numero,setNumero]=useState({valor});
+        const [activo,setActivo]=useState(false);
+
+        if(activo)
+            if (numero==2) {
+                return(
+                    <button className="border border-2 border-dark-subtle fs-2 fw-bold text-success" style={{width:50}}>
+                        numero
+                    </button>
+                )
+            }else if(numero==1){
+                return(
+                    <button className="border border-2 border-dark-subtle fs-2 fw-bold text-primary" style={{width:50}}>
+                        numero
+                    </button>
+                )
+            }else if(numero==0){
+                return(
+                    <button className="border border-2 border-dark-subtle fs-2 fw-bold text-primary" style={{width:50}}>
+                        &nbsp;
+                    </button>
+                )            
+            }else{
+                return(
+                    <button className="border border-2 border-dark-subtle fs-2 fw-bold text-dark" style={{width:50}}>
+                        numero
+                    </button>
+                )            
+            }
+        else{
             return(
                 <button className="border border-2 border-dark-subtle fs-2 fw-bold text-primary" style={{width:50}}>
                     &nbsp;
                 </button>
-            )            
-        }else{
-            return(
-                <button className="border border-2 border-dark-subtle fs-2 fw-bold text-dark" style={{width:50}}>
-                    {valor}
-                </button>
-            )            
+            )              
         }
 
 }
